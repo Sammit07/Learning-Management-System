df8803463eead375e729a515ba890e1b150cd116
Sammit Raut <sammitraut07@gmail.com>
2024-11-15 02:45:20 +1100

    Added Learning Management System project files

---
+++ studentdashboard/urls.py
@@ -0,0 +1,9 @@
+import debug_toolbar
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path("__debug__/", include("debug_toolbar.urls")),
+    path('dashboard', views.studentdashboard, name='studentdashboard'),
+
+]
